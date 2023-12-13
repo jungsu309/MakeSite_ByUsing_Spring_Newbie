4a943fe42290bedc8064714f3517f245d089d3bf
jungsu <jungsu_@naver.com>
2023-12-13 11:39:07 +0900

    Update SaveImageService.java
    이미지를 저장 할 때 path와 DB에 저장할때의 path를 다르게 저장하도록 수정.
    (html에서는 full path가 아닌 static 이후의 경로만 필요)

--- src/main/java/com/jungsu_site/jungsu_tutorial/service/SaveImageService.java
+++ src/main/java/com/jungsu_site/jungsu_tutorial/service/SaveImageService.java
@@ -1,18 +1,17 @@
 package com.jungsu_site.jungsu_tutorial.service;
 
 import com.jungsu_site.jungsu_tutorial.domain.ImageEntity;
-import com.jungsu_site.jungsu_tutorial.domain.Member;
 import com.jungsu_site.jungsu_tutorial.repository.DBBackGroundRepository;
-import com.jungsu_site.jungsu_tutorial.repository.MemberRepository;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SaveImageService {
@@ -40,21 +39,30 @@ public class SaveImageService {
 
         long fileSize = file.getSize();
         //내가 지정해주는거다.
-        Path uploadPath = Path.of("src", "main", "resources", "static", "Uploads", "BackGroundImage");
-
+        //static까지가 기본 경로이기 때문에? 그 이후부터..해주자..ㅠㅠㅠ
+//        "src", "main", "resources", "static"
+//        String uploadPath = "src/main/resources/static/Uploads/BackGroundImage";
+        //아래거는 절대경로;;
+        Path uploadPath = Paths.get("src", "main", "resources", "static", "Uploads", "BackGroundImage").toAbsolutePath();
+        //이게 상대경로
+//        Path uploadPath = Paths.get("Uploads", "BackGroundImage");
+        String str_path = "/Uploads/BackGroundImage";//localhost:8080
         ImageEntity imageEntity = new ImageEntity();
         imageEntity.setFileName(fileName);
-        imageEntity.setFilePath(uploadPath.toString());
+        imageEntity.setFilePath(str_path + "/" + fileName);//+"/"+fileName
         imageEntity.setFileSize(file.getSize());
         imageEntity.setUserName(sessionAttribute);
 
 
         // 파일 복사 -> path에 저장
         Path destinationPath = uploadPath.resolve(fileName);  // 목적지 Path 생성
+        System.out.println(destinationPath);
         Files.copy(file.getInputStream(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
         //여기까지는 ok -> 이미지가 저장되는걸로 확인
 
         bgRepository.saveToDB(imageEntity);
     }
 
+    public List<ImageEntity> findImagesById(String sessionAttribute) {
+        return bgRepository.findById(sessionAttribute);}
 }
