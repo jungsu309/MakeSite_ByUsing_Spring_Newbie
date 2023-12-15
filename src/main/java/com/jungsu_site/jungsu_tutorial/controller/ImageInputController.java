fbc09b61f76ab8f14ddfc96c91612378d890ce08
jungsu <jungsu_@naver.com>
2023-12-15 16:19:12 +0900

    file rename and modify functions
    파일 이름 변경 : BackGroundInput ->ImageInput
    서버에 파일 다운로드 하는 기능과 파일 url 전송하는 부분 분리

--- src/main/java/com/jungsu_site/jungsu_tutorial/controller/ImageInputController.java
+++ src/main/java/com/jungsu_site/jungsu_tutorial/controller/ImageInputController.java
@@ -4,6 +4,7 @@ import com.jungsu_site.jungsu_tutorial.domain.ImageEntity;
 import com.jungsu_site.jungsu_tutorial.service.MemberService;
 import com.jungsu_site.jungsu_tutorial.service.SaveImageService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,24 +12,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
-public class BackGroundInputController {
+public class ImageInputController {
 
 
     private final SaveImageService saveimageService;
     private final MemberService memberService;
 
-    public BackGroundInputController(SaveImageService saveimageService, MemberService memberService) {
+    public ImageInputController(SaveImageService saveimageService, MemberService memberService) {
         this.saveimageService = saveimageService;
         this.memberService = memberService;
     }
@@ -36,28 +33,27 @@ public class BackGroundInputController {
 
     @PostMapping("/BgImgMenu")
     public String BGMenu(){
-        return "SettingBackgroundImgMenu";
+        return "ImageInput";
     }
 
-    @PostMapping("/BgImgMenu/inputBgImg")
+    @PostMapping("/BgImgMenu/inputImg")
     @ResponseBody
-    public List<String> handleFileUpload(@RequestParam("Fileinput") MultipartFile file, HttpServletRequest request, Model model) {
+    public String FileUploadToServer(@RequestParam("Fileinput") MultipartFile file, HttpServletRequest request) throws IOException {
         // 이미지를 저장하고 DB에 저장하는 로직을 추가하세요.
         // 예를 들면, 서비스 클래스를 통해 이미지를 저장하고 DB에 저장하는 기능을 호출합니다.
         String sessionAttribute = memberService.getSessionAttribute(request, "userID");
+        saveimageService.saveImage(file, sessionAttribute);
+        return "File uploaded successfully.";
 
-        try {
-            saveimageService.saveImage(file, sessionAttribute);
-        } catch (IOException e) {
-            e.printStackTrace(); // 실제 상황에 따라 적절한 예외 처리 로직을 추가해야 합니다.
-            // 예외 처리를 위해 사용자에게 알림을 보낼 수도 있습니다.
-//            return "redirect:/BgImgMenu/error?message=Image+upload+failed"; //chatgpt가 알려준 오류알림.
-            //일단 비워두고싶음
-        }
-//        return "redirect:/BgImgMenu";
+    }
 
+    @GetMapping("/BgImgMenu/getImg")
+    public ResponseEntity<List<String>> sendFilePath(HttpServletRequest request){
+        String sessionAttribute = memberService.getSessionAttribute(request, "userID");
         // 이미지 여러개
         List<ImageEntity> ImageList = saveimageService.findImagesById(sessionAttribute);
+        System.out.println("id로 ImageEnity 정보 가져오기");
+        System.out.println(ImageList);
 
         //ajax를 이용해서 비동기적으로 전송하기 때문에 model로 보낼 필요가 없다.
 //        model.addAttribute("images", ImageList);
@@ -65,21 +61,15 @@ public class BackGroundInputController {
                 .map(ImageEntity::getFilePath) // 또는 .getImageUrl()# + "\\" + imageEntity.getFileName()
                 .collect(Collectors.toList());
 
-        System.out.println("image-list!!!!");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl("no-cache");
+
+        System.out.println("ImageEnity정보중에서 Path 정보만 가져오기");
         System.out.println(imagePathsOrUrls);
 
-        return imagePathsOrUrls;
+        return new ResponseEntity<>(imagePathsOrUrls, headers, HttpStatus.OK);
 
-//        return "SettingBackgroundImgMenu";
-//        return "Json view";
-//        return ImageList;
     }
 
-
-
-//    @GetMapping("/BgImgMenu")
-//    public String handleFileUpload_page(){
-//        return "redirect:/";
-//
-//    }
 }
