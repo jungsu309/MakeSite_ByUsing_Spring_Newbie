0c7a6aabf9e1ea1594da737843ea7d569d30d935
jungsu <jungsu309@waikorea.com>
2024-01-03 18:04:14 +0900

    websocket 사용하는 코드 폐기
    이미지를 뿌리는 데 굳이 websocket이 필요하지 않음.

--- src/main/java/com/jungsu_site/jungsu_tutorial/controller/ImageInputController.java
+++ src/main/java/com/jungsu_site/jungsu_tutorial/controller/ImageInputController.java
@@ -4,30 +4,30 @@ import com.jungsu_site.jungsu_tutorial.domain.ImageEntity;
 import com.jungsu_site.jungsu_tutorial.service.MemberService;
 import com.jungsu_site.jungsu_tutorial.service.SaveImageService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
+//@RestController
 public class ImageInputController {
 
 
     private final SaveImageService saveimageService;
     private final MemberService memberService;
 
-    public ImageInputController(SaveImageService saveimageService, MemberService memberService) {
+    //실시간 통신을 위한 websocket
+//    private final SimpMessagingTemplate messagingTemplate;
+
+    public ImageInputController(SaveImageService saveimageService, MemberService memberService) {// SimpMessagingTemplate messagingTemplate
         this.saveimageService = saveimageService;
         this.memberService = memberService;
+//        this.messagingTemplate = messagingTemplate;
     }
 
 
@@ -43,12 +43,14 @@ public class ImageInputController {
         // 예를 들면, 서비스 클래스를 통해 이미지를 저장하고 DB에 저장하는 기능을 호출합니다.
         String sessionAttribute = memberService.getSessionAttribute(request, "userID");
         saveimageService.saveImage(file, sessionAttribute);
+
         return "File uploaded successfully.";
 
     }
 
     @GetMapping("/BgImgMenu/getImg")
-    public ResponseEntity<List<String>> sendFilePath(HttpServletRequest request){
+    @ResponseBody
+    public List<String> sendFilePath(HttpServletRequest request){
         String sessionAttribute = memberService.getSessionAttribute(request, "userID");
         // 이미지 여러개
         List<ImageEntity> ImageList = saveimageService.findImagesById(sessionAttribute);
@@ -62,14 +64,16 @@ public class ImageInputController {
                 .collect(Collectors.toList());
 
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setCacheControl("no-cache");
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setCacheControl("no-cache");
 
         System.out.println("ImageEnity정보중에서 Path 정보만 가져오기");
         System.out.println(imagePathsOrUrls);
 
-        return new ResponseEntity<>(imagePathsOrUrls, headers, HttpStatus.OK);
+        return imagePathsOrUrls;
 
     }
 
 }
+// WebSocket으로 클라이언트에게 파일 경로 전송
+//        messagingTemplate.convertAndSend("/topic/images", Collections.singletonList(savedFilePath));
